a3de4db7b78fb853aa912415402a544d67856d76
ftpggversed <ggversed.ttv@gmail.com>
2024-08-11 11:29:35 -0700

    first commit

---
+++ src/mongodb.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/loginform")
+.then(() => {
+    console.log("Mongo Connected On Port 27017")
+})
+.catch(() => {
+    console.log("Failed To Connect Mongo On Port 27017")
+})
+
+const LogInSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+
+const collection = new mongoose.model("Collection1", LogInSchema)
+
+moduule.exports = collection
